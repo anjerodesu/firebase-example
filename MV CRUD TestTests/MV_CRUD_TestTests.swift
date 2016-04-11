43b934f373e388c3447059faf0bd5be8ca64c79a
Angelo Villegas <gelo.web@gmail.com>
2016-04-11 18:03:01 +0800

    Initial Commit

---
+++ MV CRUD TestTests/MV_CRUD_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  MV_CRUD_TestTests.swift
+//  MV CRUD TestTests
+//
+//  Created by Angelo Villegas on 11/4/16.
+//  Copyright © 2016 Angelo Villegas. All rights reserved.
+//
+
+import XCTest
+@testable import MV_CRUD_Test
+
+class MV_CRUD_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
